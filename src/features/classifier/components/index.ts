feab2f1f119c1339a886984d8f72d5780274535e
Bouchdak <info@bluelineccs.nl>
2025-10-08 21:42:44 +0200

    chore: finalize ResultPanel + barrel updates

--- src/features/classifier/components/index.ts
+++ src/features/classifier/components/index.ts
@@ -1,21 +1,5 @@
-export { default as HeaderBar } from "./components/HeaderBar";
-export { default as PresetsCard } from "./components/PresetsCard";
-export { default as InputsForm } from "./components/InputsForm";
-export { default as ResultPanel } from "./components/ResultPanel";
-export { default as ScenarioCompare } from "./components/ScenarioCompare";
-
-export type {
-  ClassifierInput,
-  MultiOptions,
-  ScoreBreakdown,
-  ClassifierResult,
-  Preset,
-} from "./types";
-
-export { computeScore } from "./services/computeScore";
-export {
-  listPresets,
-  createPreset,
-  updatePreset,
-  deletePreset,
-} from "./services/presetsStore";
+export { default as HeaderBar } from './HeaderBar';
+export { default as PresetsCard } from './PresetsCard';
+export { default as InputsForm } from './InputsForm';
+export { default as ResultPanel } from './ResultPanel';
+export { default as ScenarioCompare } from './ScenarioCompare';
